6076676524ba0a548c6bf786093c11b37f67d5cd
AhmeddAymannHalim <153849097+AhmeddAymannHalim@users.noreply.github.com>
2024-09-20 00:27:05 +0300

    Part 01 Department Repository - DAL

---
+++ LinkDev.IKEA.DAL/Persistance/Repositories/Departments/DepartmentRepository.cs
@@ -0,0 +1,61 @@
+﻿using LinkDev.IKEA.DAL.Models.Department;
+using LinkDev.IKEA.DAL.Persistance.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistance.Repositories.Departments
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public DepartmentRepository(ApplicationDbContext dbContext)//ASK CLR TO MAKE OBJECT FROM APPLICATIONDBCONTEXT
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Department> GetAll(bool withAsNoTracking = false)
+        {
+            if (withAsNoTracking)
+                return _dbContext.Departments.AsNoTracking().ToList();
+
+            return _dbContext.Departments.ToList();
+        }
+
+
+        public Department? GetById(int id)
+        {
+            // var department = _dbContext.Departments.Local.FirstOrDefault(D => D.Id == id);
+            // return department;
+
+            return _dbContext.Departments.Find(id);
+        }
+
+        public int Add(Department entity)
+        {
+            _dbContext.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Update(Department entity)
+        {
+            _dbContext.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Department entity)
+        {
+            _dbContext.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+       
+
+
+        
+    }
+}
--- LinkDev.IKEA.PL/Program.cs
+++ LinkDev.IKEA.PL/Program.cs
@@ -1,5 +1,7 @@
 using LinkDev.IKEA.DAL.Persistance.Data;
+using LinkDev.IKEA.DAL.Persistance.Repositories.Departments;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace LinkDev.IKEA.PL
 {
@@ -16,20 +18,27 @@ namespace LinkDev.IKEA.PL
             // builder.Services.AddScoped<ApplicationDbContext>();
             // builder.Services.AddScoped<DbContextOptions<ApplicationDbContext>>(ServerProvider =>
             // {
-            //     var options = new DbContextOptions<ApplicationDbContext>();
-            // 
+            //     
+            //     
             //     var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
             // 
-            //     options = optionsBuilder.Options;
+            //     optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             // 
-            //     return options;
+            // 
+            //     //var scope = ServerProvider.CreateScope();
+            // 
+            //     //var department = scope.ServiceProvider.GetService<IDepartmentRepository>();
+            //    
+            // 
+            //     return optionsBuilder.Options;
             // 
             //     
             // });
 
-            builder.Services.AddDbContext<ApplicationDbContext>(optionBuilder =>
-            {
-                optionBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+              builder.Services.AddDbContext<ApplicationDbContext>(ServerProvider =>
+              {
+                  ServerProvider.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+              });
                 //ByDefault Scoped
                 // contextLifetime:ServiceLifetime.Scoped,
                 // optionsLifetime:ServiceLifetime.Scoped,
