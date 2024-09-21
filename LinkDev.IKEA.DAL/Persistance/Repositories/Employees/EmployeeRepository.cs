eb4395e999b0935ffb4f2d9bca6f659625aa904c
AhmeddAymannHalim <153849097+AhmeddAymannHalim@users.noreply.github.com>
2024-09-21 04:12:42 +0300

    Part 04 Employee Module - Repository

---
+++ LinkDev.IKEA.DAL/Persistance/Repositories/Employees/EmployeeRepository.cs
@@ -0,0 +1,24 @@
+﻿using LinkDev.IKEA.DAL.Entities.Department;
+using LinkDev.IKEA.DAL.Entities.Employee;
+using LinkDev.IKEA.DAL.Persistance.Data;
+using LinkDev.IKEA.DAL.Persistance.Repositories._Generic;
+using LinkDev.IKEA.DAL.Persistance.Repositories.Departments;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistance.Repositories.Employees
+{
+    public class EmployeeRepository : GenericRepository<Employee>,IEmployeeRepository
+    {
+
+        public EmployeeRepository(ApplicationDbContext dbContext):base(dbContext)//ASK CLR OBJECT FROM CLASS DBCONTEXT
+        {
+            
+        }
+
+    }
+}
