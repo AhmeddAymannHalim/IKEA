7e6a26408ba78f0d633b6ee06bcdc72621888c40
AhmeddAymannHalim <153849097+AhmeddAymannHalim@users.noreply.github.com>
2024-09-23 03:31:33 +0300

    Part 09 Employee Controller - Edit

--- LinkDev.IKEA.BLL/Models/Employees/UpdatedEmployeeDto.cs
+++ LinkDev.IKEA.BLL/Models/Employees/UpdatedEmployeeDto.cs
@@ -12,18 +12,24 @@ namespace LinkDev.IKEA.BLL.Models.Employees
     {
         public int Id { get; set; }
 
-
         //[Required]
         [MaxLength(50, ErrorMessage = "Max Length Of Name is 50 chars")]
-        [MinLength(20, ErrorMessage = "Min Length Of Name is 5 chars")]
+        [MinLength(5, ErrorMessage = "Min Length Of Name is 5 chars")]
         public string Name { get; set; } = null!;
 
         [Range(22, 30)]
         public int? Age { get; set; }
 
-        [RegularExpression("@^[0-9]{1,3}-[a-zA-Z]{5,10}-[a-zA-Z]{4,10}-[a-zA-Z]{5,10}$",
-                          ErrorMessage = "Address must be like 123-Street-City-Country")]
+  
+
+
+        [RegularExpression(@"^[0-9]{1,3}-[a-zA-Z]{5,10}-[a-zA-Z]{4,10}-[a-zA-Z]{5,10}$",
+                         ErrorMessage = "Address must be like 123-Street-City-Country")]
         public string Address { get; set; } = null!;
+
+        [Display(Name = "Creation Date")]
+        public DateOnly CreationDate { get; set; }
+
 
         [DataType(DataType.Currency)]
         public decimal Salary { get; set; }
@@ -31,15 +37,16 @@ namespace LinkDev.IKEA.BLL.Models.Employees
         [Display(Name = "Is Active")]
         public bool IsActive { get; set; }
 
-        [EmailAddress]
-        public string? Email { get; set; }
-
         [Display(Name = "Phone Number")]
         [Phone]
         public string? PhoneNumber { get; set; }
 
         [Display(Name = "Hiring Date")]
         public DateOnly HiringDate { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
 
         public Gender Gender { get; set; }
 
